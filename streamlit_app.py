2912bbc9384a58293c732f2cdc92dcfb6976a5a5
bmszone42 <122175396+bmszone42@users.noreply.github.com>
2023-08-04 19:14:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -161,7 +161,8 @@ def perform_analysis(symbol, data, color, new_df, weeks):
                 # Add the price and percentage change to the DataFrame
                 dates_with_prices[f'Price at {week} Weeks'] = prices_shifted.loc[div_dates, 'Close'].values
                 dates_with_prices[f'Change at {week} Weeks (%)'] = ((dates_with_prices[f'Price at {week} Weeks'] - dates_with_prices['Closing Price']) / dates_with_prices['Closing Price']) * 100
-    
+
+
             st.write("Dividend Dates with More Closing Prices:")
             st.write(dates_with_prices)
 
